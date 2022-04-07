03f0b26da3ba227ca1a04474e366b903ef52d6e8
Anikroy7 <roy645800@gmail.com>
2022-04-07 17:44:09 +0600

    registration form  added from react bootsrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { getAuth } from "firebase/auth";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import app from './firebase.init';
+import Form from 'react-bootstrap/Form'
+import { Button } from "react-bootstrap";
 
 const auth = getAuth(app)
 
@@ -23,13 +26,27 @@ const handelFromSubmit = event => {
 function App() {
   return (
     <div className="App">
-      <form onSubmit={handelFromSubmit}>
-        <input onBlur={handelEmailBlur} type="email" name="" id="" /><br />
-        <input onBlur={handelPasswordBlur} type="password" name="" id="" /><br />
+      <h2 className="text-secondary">Register Please !!</h2>
+      <section className="w-50 mx-auto mt-4 border p-2 rounded">
 
-        <input type="submit" value="login" />
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-      </form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </section>
     </div>
   );
 }
