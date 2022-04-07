9e5f456a4df31ae1e788b4fd62e340d004c5e96b
Anikroy7 <roy645800@gmail.com>
2022-04-07 17:54:50 +0600

    handeler added by onBlur

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ function App() {
       <h2 className="text-secondary">Register Please !!</h2>
       <section className="w-50 mx-auto mt-4 border p-2 rounded">
 
-        <Form>
+        <Form onSubmit={handelFromSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -40,7 +40,7 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Password" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
