11981caf5baadc15c199c8ec81b58dd2f2aabf40
Anikroy7 <roy645800@gmail.com>
2022-04-07 17:31:11 +0600

    get input value by onBlur event

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import app from './firebase.init';
 
 const auth = getAuth(app)
 
+const handelEmailBlur = event => {
+  console.log(event.target.value);
+}
+
+
+const handelPasswordBlur = event => {
+
+  console.log(event.target.value);
+}
+
+const handelFromSubmit = event => {
+  console.log('anik submited');
+  event.preventDefault();
+}
+
+
 function App() {
   return (
     <div className="App">
-      <input type="email" name="" id="" />
-      <input type="password" name="" id="" />
+      <form onSubmit={handelFromSubmit}>
+        <input onBlur={handelEmailBlur} type="email" name="" id="" /><br />
+        <input onBlur={handelPasswordBlur} type="password" name="" id="" /><br />
+
+        <input type="submit" value="login" />
+
+      </form>
     </div>
   );
 }
